2233ab8e44d33cb12abc4b5f4c14a311815e007a
Vladislav Tsurikov <uladzislau78@gmail.com>
2026-03-13 18:25:25 +0300

    [Remove] old scripts

--- Assets/Assemblies/AutoStrike/Runtime/Player/Actions/ReadInputAction.cs
+++
@@ -1,9 +0,0 @@
-using VladislavTsurikov.EntityDataAction.Runtime.Core;
-
-namespace AutoStrike.Actions
-{
-    // Legacy shim kept only so old serialized prefab actions can be loaded and removed safely by SyncToTypesIfNeeded.
-    public sealed class ReadInputAction : EntityMonoBehaviourAction
-    {
-    }
-}
--- Assets/Assemblies/AutoStrike/Runtime/Player/Data/PlayerInputData.cs
+++
@@ -1,9 +0,0 @@
-using VladislavTsurikov.Nody.Runtime.Core;
-
-namespace AutoStrike.Data
-{
-    // Legacy shim kept only so old serialized prefab data can be loaded and removed safely by SyncToTypesIfNeeded.
-    public sealed class PlayerInputData : ComponentData
-    {
-    }
-}
